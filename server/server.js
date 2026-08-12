ae10671cc76b74cdd457148e77ba896bd236bbea
unknown <aditya.kokate47@gmail.com>
2026-08-12 13:38:57 +0530

    Fix Express 5 wildcard routing error

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/api/prompts',promptRoutes)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 } else {
