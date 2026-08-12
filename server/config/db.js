434581074046f5464f12e2a4be7db19250b32242
unknown <aditya.kokate47@gmail.com>
2026-08-12 13:44:36 +0530

    Fix database connection variable error

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose")
 
 const connectDb = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL)
+        const uri = process.env.MONGODB_URL || process.env.MONGO_URI;
+        await mongoose.connect(uri)
         console.log("Mongo DB connected")
     }catch(e){
-        console.error("MongoDb not connected",error.message);
+        console.error("MongoDb not connected", e.message);
         process.exit(1);
     }
 }
